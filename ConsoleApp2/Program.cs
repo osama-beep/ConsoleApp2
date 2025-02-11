a0db12d84376b4a40be2c8cc54a8d45ea5dec07d
vintry <oennabati3@gmail.com>
2025-02-11 13:17:35 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+var persona = new Persona
+{
+    Nome = "Luisa",
+    Cognome = "Marcon",
+    Eta = 26
+};
+
+Console.WriteLine(persona.GetDettagli());
+
+public class Persona
+{
+    public string Nome { get; set; }
+    public string Cognome { get; set; }
+    public int Eta { get; set; }
+
+    public string GetNome()
+    {
+        return Nome;
+    }
+
+    public string GetCognome()
+    {
+        return Cognome;
+    }
+
+    public int GetEta()
+    {
+        return Eta;
+    }
+
+    public string GetDettagli()
+    {
+        return $"Nome: {Nome}, Cognome: {Cognome}, Età: {Eta}";
+    }
+
+
+}
